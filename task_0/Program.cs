18c2326fa711301db14d0e356e62446d1e510da5
DmitryiKD <dimka_2284@mail.ru>
2022-11-28 23:02:55 +0300

    Added task 0, 1, 3

---
+++ task_0/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int dataEntryXY (string str)
+{
+    Console.Write (str);
+
+    int number = int.Parse(Console.ReadLine());
+
+    return number;
+}
+
+void findQuadrant (int x, int y)
+{
+    if (x > 0 && y > 0) Console.WriteLine ("Это первый квадрант");
+
+    if (x < 0 && y > 0) Console.WriteLine ("Это второй квадрант");
+
+    if (x < 0 && y < 0) Console.WriteLine ("Это третий квадрант");
+
+    if (x > 0 && y < 0) Console.WriteLine ("Это четвертый квадрант");
+}
+
+
+
+
+
+
+int x = dataEntryXY ("Введите координаты X = : ");
+
+
+int y = dataEntryXY ("Введите координаты Y = : ");
+
+
+findQuadrant (x, y);
---
+++ task_3/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+int dataEntryXY (string str)
+{
+    Console.Write (str);
+
+    int number = int.Parse(Console.ReadLine());
+
+    return number;
+}
+
+void findXY (int x)
+{
+    if (x == 1) Console.WriteLine ("X > 0, Y > 0");
+
+    if (x == 2) Console.WriteLine ("X < 0, Y > 0");
+
+    if (x == 3) Console.WriteLine ("X < 0, Y < 0");
+
+    if (x == 4) Console.WriteLine ("X > 0, Y < 0");
+}
+
+
+int x = dataEntryXY ("Введите номер квадранта: ");
+
+findXY (x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
